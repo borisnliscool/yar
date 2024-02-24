7c6c177277d5188ce1aad89e75b6cfdef59db37b
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-02-25 00:56:35 +0100

    chore: fix tailwind importing for ui components

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	// I'm pretty sure there's a better way of doing this, but can't be bothered to figure that out
+	content: ['./src/**/*.{html,js,svelte,ts}', '../../packages/ui/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
--- packages/ui/components/MyCounterButton.svelte
+++ packages/ui/components/MyCounterButton.svelte
@@ -9,6 +9,7 @@
 </script>
 
 <Icon icon="fa6-solid:house" />
+
 <button class="bg-red-500" on:click={handleClick}>
 	clicks: {count}
 </button>
