a115725a97751292a7d0a7f27db1476911575a63
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-02-29 21:45:43 +0100

    chore: update upload page styles

--- apps/web/src/routes/upload/+page.svelte
+++ apps/web/src/routes/upload/+page.svelte
@@ -4,10 +4,10 @@
 	import { Button, Input } from '@repo/ui';
 	import { instant } from '@repo/utils';
 	import humanFormat from 'human-format';
+	import VideoFormat from './VideoFormat.svelte';
 
 	import dayjs from 'dayjs';
 	import duration from 'dayjs/plugin/duration';
-	import VideoFormat from './VideoFormat.svelte';
 
 	dayjs.extend(duration);
 
@@ -36,56 +36,65 @@
 	});
 </script>
 
-<div class="mx-auto flex w-full max-w-2xl flex-col gap-4 py-4">
-	<div class="flex items-center gap-2">
-		<Input placeholder="Video URL" bind:value={input} on:paste={search} />
-		<Button on:click={search} disabled={searching}>Search</Button>
-	</div>
+<div
+	class="grid h-screen max-h-screen grid-cols-5 gap-4 bg-neutral-100 p-4 text-black dark:bg-neutral-900 dark:text-white"
+>
+	<div class="h-full w-full rounded-lg bg-white p-4 shadow dark:bg-neutral-800">Download Queue</div>
+	<div
+		class="col-span-4 grid h-full grid-cols-2 gap-4 overflow-y-auto rounded-lg bg-white p-4 shadow dark:bg-neutral-800"
+	>
+		<div class="flex h-full flex-col gap-4">
+			<div class="flex items-center gap-2">
+				<Input placeholder="Video URL" bind:value={input} on:paste={search} />
+				<Button class="px-4" on:click={search} disabled={searching}>Search</Button>
+			</div>
 
-	<div class="flex flex-col gap-4">
-		{#await videoInfo}
-			loading..
-		{:then videoData}
-			{#if videoData}
-				<img
-					src={videoData.thumbnail}
-					alt="video thumbnail"
-					class="aspect-video w-full overflow-hidden rounded bg-neutral-400 object-contain dark:bg-neutral-700"
-				/>
+			<div class="flex flex-col gap-4">
+				{#await videoInfo}
+					loading..
+				{:then videoData}
+					{#if videoData}
+						<img
+							src={videoData.thumbnail}
+							alt="video thumbnail"
+							class="aspect-video w-full overflow-hidden rounded bg-neutral-400 object-contain dark:bg-neutral-700"
+						/>
 
-				<h1 class="text-2xl font-semibold">{videoData.fulltitle}</h1>
+						<h1 class="text-2xl font-semibold">{videoData.fulltitle}</h1>
 
-				<div class="flex flex-col">
-					{#if videoData.duration}
-						<p>
-							Duration: <span class="font-semibold">
-								{dayjs.duration(videoData.duration, 'seconds').format('HH:mm:ss')}
-							</span>
-						</p>
+						<div class="flex flex-col">
+							{#if videoData.duration}
+								<p>
+									Duration: <span class="font-semibold">
+										{dayjs.duration(videoData.duration, 'seconds').format('HH:mm:ss')}
+									</span>
+								</p>
+							{/if}
+
+							{#if videoData.filesize_approx}
+								<p>
+									File size approximate: <span class="font-semibold">
+										{humanFormat(videoData.filesize_approx)}
+									</span>
+								</p>
+							{/if}
+						</div>
+
+						<div class="flex flex-col gap-2">
+							<p class="font-semibold">Formats</p>
+							<div class="flex max-h-96 flex-col gap-1 overflow-y-auto pr-1">
+								{#each videoData.formats as format}
+									<VideoFormat {format} />
+								{/each}
+							</div>
+						</div>
 					{/if}
+				{:catch error}
+					failed! {error}
+				{/await}
+			</div>
+		</div>
 
-					{#if videoData.filesize_approx}
-						<p>
-							File size approximate: <span class="font-semibold">
-								{humanFormat(videoData.filesize_approx)}
-							</span>
-						</p>
-					{/if}
-				</div>
-
-				<div class="flex flex-col gap-2">
-					<p class="font-semibold">Formats</p>
-					<div class="flex max-h-96 flex-col gap-1 overflow-y-auto pr-1">
-						{#each videoData.formats as format}
-							<VideoFormat {format} />
-						{/each}
-					</div>
-				</div>
-
-				<!-- <pre>{JSON.stringify(videoData, null, 4)}</pre> -->
-			{/if}
-		{:catch error}
-			failed! {error}
-		{/await}
+		<div class="h-full rounded border p-4">Progress logs</div>
 	</div>
 </div>
