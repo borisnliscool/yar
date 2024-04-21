6cd0116b6bd73f9f66446659aba715d8c9d15c5e
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-04-21 12:49:04 +0200

    chore: update database url

--- apps/backend/src/services/databaseService.ts
+++ apps/backend/src/services/databaseService.ts
@@ -1,2 +1,7 @@
 import { PrismaClient } from '@repo/database';
-export const database = new PrismaClient();
+
+export const database = new PrismaClient({
+	datasourceUrl: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : undefined,
+});
+
+database.$connect().catch(console.error);
