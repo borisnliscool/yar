0304fbd19f3a12b695aa3d800cbbef6d5b1e9537
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2024-05-30 22:11:35 +0200

    wip: update bump-package-version

--- .github/actions/bump-package-version.js
+++ .github/actions/bump-package-version.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 
 const version = process.env.VERSION;
-const package = require('package.json');
+const packagePath = 'package.json';
 
+const package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 package.version = version;
-fs.writeFileSync('package.json', JSON.stringify(package, null, 4));
+
+fs.writeFileSync(packagePath, JSON.stringify(package, null, 4));
 console.log(`Bumped package version to ${version}.`);
